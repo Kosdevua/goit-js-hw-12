8f9cb37bce7b9e4a60b3b7b47ba9c949e10edb53
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-15 23:43:30 +0300

    added logic

--- src/js/pixarbay-api.js
+++ src/js/pixarbay-api.js
@@ -1,6 +1,8 @@
 // У файлі pixabay-api.js зберігай функції для HTTP-запитів.
 
-export function getPhotos(q) {
+//У разі пошуку за новим ключовим словом значення page потрібно повернути до початкового, оскільки буде пагінація для нової колекції зображень.
+
+export function getPhotos(q, page) {
   const baseUrl = 'https://pixabay.com/api';
   const API_KEY = '43362047-b69de0f8b7d88fd6ec05c5589';
 
@@ -10,7 +12,12 @@ export function getPhotos(q) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page,
+    per_page: 15,
   });
+
+  page++;
+
   return fetch(`${baseUrl}/?${params}`).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
@@ -19,3 +26,25 @@ export function getPhotos(q) {
     return res.json();
   });
 }
+
+////====////
+
+// export function getPhotos(q) {
+//   const baseUrl = 'https://pixabay.com/api';
+//   const API_KEY = '43362047-b69de0f8b7d88fd6ec05c5589';
+
+//   const params = new URLSearchParams({
+//     key: API_KEY,
+//     q,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   });
+//   return fetch(`${baseUrl}/?${params}`).then(res => {
+//     if (!res.ok) {
+//       throw new Error(res.status);
+//       //   if(res)
+//     }
+//     return res.json();
+//   });
+// }
--- src/main.js
+++ src/main.js
@@ -5,25 +5,82 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createGalleryMarkup } from './js/render-functions';
 import { getPhotos } from './js/pixarbay-api';
 
+import axios from 'axios';
+
 const formElement = document.querySelector('[name="search"]');
 const inputElement = document.querySelector('input');
 const galleryArea = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 let lightbox;
+const btnLoadMore = document.querySelector('.js-button-load');
+let searchQuery = null;
+let page = 1;
+
+if (!lightbox) {
+  lightbox = new SimpleLightbox('.gallery a');
+} else {
+  lightbox.refresh();
+}
+
+const limit = 15;
+const totalImages = null;
+const lastPage = Math.ceil(totalImages / 15);
 
 formElement.addEventListener('submit', onSubmit);
+btnLoadMore.addEventListener('click', loadedMoreImages);
+
+function loadedMoreImages() {
+  page += 1;
+
+  getPhotos(searchQuery, page)
+    .then(res => {
+      galleryArea.insertAdjacentHTML(
+        'beforeend',
+        createGalleryMarkup(res.hits)
+      );
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+      const lastPage = Math.ceil(res.total / 15);
+      ///????
+      if (lastPage === page) {
+        btnLoadMore.classList.add('is-hidden');
+        alert("that's all!!!");
+        // iziToast;
+      }
+    })
+    .catch(console.log);
+}
+
+////----////
+
+////----////
 
 function onSubmit(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.elements.search.value.trim();
 
+  searchQuery = event.currentTarget.elements.search.value.trim();
+  showLoader();
+  btnLoadMore.classList.add('is-hidden');
   galleryArea.innerHTML = '';
+  page = 1;
 
   if (!searchQuery) return;
-  showLoader();
 
-  getPhotos(searchQuery)
+  getPhotos(searchQuery, page) //
     .then(res => {
+      if (res.total > 0) {
+        iziToast.info({
+          position: 'topRight',
+          message: `We found ${res.total} images`,
+          maxWidth: '432',
+        });
+      }
       if (res.hits.length === 0) {
         return iziToast.error({
           position: 'topRight',
@@ -38,10 +95,8 @@ function onSubmit(event) {
 
       galleryArea.innerHTML = createGalleryMarkup(res.hits);
 
-      if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a');
-      } else {
-        lightbox.refresh();
+      if (res.totalHits > 15) {
+        btnLoadMore.classList.remove('is-hidden');
       }
     })
     .catch(err => {
@@ -52,6 +107,8 @@ function onSubmit(event) {
     });
   inputElement.value = '';
 }
+
+////----////
 
 function showLoader() {
   loader.classList.remove('is-hidden');
